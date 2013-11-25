c09a267910450863c6d916f582292c1a6761de53
Anjana Vakil <anjanav@coli.uni-saarland.de>
2013-11-25 23:33:54 +0100

    Add project for Testing with stub test method
    Followed instructions on Unit Testing from MSDN:
    http://msdn.microsoft.com/en-us/library/hh598960.aspx

---
+++ src/lex4all_Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lex4allUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
