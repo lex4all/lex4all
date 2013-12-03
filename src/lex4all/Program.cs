ad8cd1b1af4515a3489e41285613613f5f29cf69
Anjana Sofia Vakil <anjanav@coli.uni-saarland.de>
2013-12-03 15:34:30 -0800

    Documentation for Program methods

--- src/lex4all/Program.cs
+++ src/lex4all/Program.cs
@@ -30,6 +30,10 @@ namespace lex4all
 
         }
 
+        /// <summary>
+        /// Gets the user input of words and audio files
+        /// </summary>
+        /// <returns>Dictionary with words (orthographic forms) as keys and arrays of wav file paths as values</returns>
         static Dictionary<String, String[]> GatherData()
         {
             Dictionary<String, String[]> dataDict = new Dictionary<string,string[]>();
@@ -62,6 +66,11 @@ namespace lex4all
             return dataDict;
         }
 
+        /// <summary>
+        /// Writes a dictionary of word:pronunciations kvps to an XML file matching PLS
+        /// </summary>
+        /// <param name="vocabDict">Dictionary with words (graphemes) as keys and pronunciations (phonemes) as values</param>
+        /// <param name="filename">Desired name for the output file</param>
         static void WriteLexicon(Dictionary<String, String[]> vocabDict, String filename)
         {
             Console.Write("Writing lexicon to {0}... ", filename);
@@ -80,6 +89,11 @@ namespace lex4all
             Console.WriteLine("Done.");
         }
 
+        /// <summary>
+        /// Passes the training audio files for each word to the Algorithm module for pronunciation generation
+        /// </summary>
+        /// <param name="wavDict">Dictionary with words (orthographic forms) as keys and arrays of wav file paths as values</param>
+        /// <returns>Dictionary with words (graphemes) as keys and pronunciations (phonemes) as values</returns>
         static Dictionary<String, String[]> GetLexDict(Dictionary<String, String[]> wavDict)
         {
             Dictionary<String, String[]> lexDict = new Dictionary<string,string[]>();
