32d6f709733322f572acee44c39a73275113b1db
Anjana Sofia Vakil <anjanav@coli.uni-saarland.de>
2014-01-14 19:54:59 +0100

    Make evaluation a class in lex4all instead of its own project

---
+++ src/lex4all/Evaluation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Speech.Recognition;
+
+namespace lex4all
+{
+    public static class Evaluation
+    {
+
+        /// <summary>
+        /// Performs recognition and computes accuracy on the given testing files.
+        /// </summary>
+        /// <param name="TestDict">Keys are words, values are lists of audio file names</param>
+        public static void Test(Dictionary<String, String[]> testDict, string evalGramFilename)
+        {
+            SpeechRecognitionEngine engine;
+            int total = testDict.Keys.Count;
+            int correct = 0;
+            int incorrect = 0;
+            int unrec = 0;
+            Dictionary<string, Dictionary<string, int>> confusion = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach (string word in testDict.Keys)
+            {
+                string actual = word;
+                foreach (string wavfile in testDict[word])
+                {
+                    engine = lex4all.EngineControl.getEngine();
+                    Grammar evalGram = new Grammar(evalGramFilename);
+                    engine.LoadGrammar(evalGram);
+                    engine.SetInputToWaveFile(wavfile);
+
+                    Console.Write(actual + " ");
+                    RecognitionResult result = engine.Recognize();
+
+                    if (result == null)
+                    {
+                        Console.WriteLine("unrecognized");
+                        unrec++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(result.Text + " " + result.Confidence);
+                        if (result.Text == actual)
+                        {
+                            correct++;
+
+                        }
+                        else
+                        {
+                            incorrect++;
+                        }
+                    }
+
+                }
+            }
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
--- src/lex4allEval/Program.cs
+++ src/lex4allEval/Program.cs
@@ -3,33 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Speech;
+using Microsoft.Speech.Recognition;
 
 namespace lex4allEval
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
 
-        /// <summary>
-        /// Generates a lexion from the given training files for each Yoruba word.
-        /// </summary>
-        /// <param name="TrainDict">Keys are words, values are lists of audio file names</param>
-        static void Train(Dictionary<String,List<String>> TrainDict)
-        {
-            throw new NotImplementedException();
-        }
 
-        /// <summary>
-        /// Performs recognition and computes accuracy on the given testing files for each Yoruba word.
-        /// </summary>
-        /// <param name="TestDict">Keys are words, values are lists of audio file names</param>
-        static void Test(Dictionary<String, List<String>> TestDict)
-        {
-            throw new NotImplementedException();
-        }
+
+        
 
     }
 }
