9a6e88d4c80a4ed7f2ec13889d4800608f210265
Anjana Sofia Vakil <anjanav@coli.uni-saarland.de>
2014-01-17 22:47:37 +0100

    Parse lexicon file to get list of words

--- src/lex4all/Evaluation.cs
+++ src/lex4all/Evaluation.cs
@@ -3,18 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using Microsoft.Speech.Recognition;
+using System.Diagnostics;
 
 namespace lex4all
 {
     public static class Evaluation
     {
+        /// <summary>
+        /// Get the set of words (graphemes) in the given lexicon file.
+        /// Lexicon file must use the namespace below.
+        /// </summary>
+        /// <param name="lexFileName">Path to lexicon file</param>
+        /// <returns>Array of words</returns>
+        public static string[] ReadLexicon(string lexFileName)
+        {
+       
+
+            Debug.WriteLine("Starting to read lexicon");
+            XDocument lexDoc = XDocument.Load(lexFileName);
+            Debug.WriteLine("lexdoc created");
+
+            XNamespace ns = "http://www.w3.org/2005/01/pronunciation-lexicon";
+
+            IEnumerable<XElement> childList =
+                from el in lexDoc.Root.Elements(ns + "lexeme")
+                select el.Element(ns + "grapheme");
+            foreach (XElement e in childList)
+                Debug.WriteLine(e.Value);
+            
+            string[] words =
+                (from lexeme in lexDoc.Root.Elements(ns+"lexeme")
+                select lexeme.Element(ns+"grapheme").Value).ToArray();
+
+            Debug.WriteLine(String.Join(",", words));
+
+            return words;
+
+            
+        }
 
         /// <summary>
         /// Performs recognition and computes accuracy on the given testing files.
         /// </summary>
         /// <param name="TestDict">Keys are words, values are lists of audio file names</param>
-        public static void Test(Dictionary<String, String[]> testDict, string evalGramFilename)
+        public static void Evaluate(Dictionary<String, String[]> testDict, string evalGramFilename)
         {
             SpeechRecognitionEngine engine;
             int total = testDict.Keys.Count;
