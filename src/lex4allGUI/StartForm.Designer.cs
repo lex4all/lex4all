ca01a8fe5ff911efee36fd51dc528ab611b05556
Anjana Sofia Vakil <anjanav@coli.uni-saarland.de>
2014-01-18 20:55:33 +0100

    Make StartForm prettier (#37)

--- src/lex4allGUI/StartForm.Designer.cs
+++ src/lex4allGUI/StartForm.Designer.cs
@@ -43,47 +43,54 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Controls.Add(this.buildLexBtn, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.evalBtn, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 1, 4);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buildLexBtn, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.evalBtn, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 89F));
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.0596F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.9404F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 311);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // buildLexBtn
             // 
-            this.buildLexBtn.Location = new System.Drawing.Point(31, 57);
+            this.buildLexBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buildLexBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buildLexBtn.Location = new System.Drawing.Point(31, 93);
             this.buildLexBtn.Name = "buildLexBtn";
-            this.buildLexBtn.Size = new System.Drawing.Size(137, 23);
+            this.buildLexBtn.Size = new System.Drawing.Size(221, 83);
             this.buildLexBtn.TabIndex = 1;
-            this.buildLexBtn.Text = "Build new lexicon";
+            this.buildLexBtn.Text = "Build a new lexicon";
             this.buildLexBtn.UseVisualStyleBackColor = true;
             this.buildLexBtn.Click += new System.EventHandler(this.buildLexBtn_Click);
             // 
             // evalBtn
             // 
-            this.evalBtn.Location = new System.Drawing.Point(31, 111);
+            this.evalBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.evalBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evalBtn.Location = new System.Drawing.Point(31, 198);
             this.evalBtn.Name = "evalBtn";
-            this.evalBtn.Size = new System.Drawing.Size(174, 23);
+            this.evalBtn.Size = new System.Drawing.Size(221, 38);
             this.evalBtn.TabIndex = 2;
-            this.evalBtn.Text = "Evaluate existing lexicon";
+            this.evalBtn.Text = "Evaluate an existing lexicon";
             this.evalBtn.UseVisualStyleBackColor = true;
             this.evalBtn.Click += new System.EventHandler(this.evalBtn_Click);
             // 
             // linkLabel1
             // 
+            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(31, 241);
+            this.linkLabel1.Location = new System.Drawing.Point(85, 274);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(113, 13);
             this.linkLabel1.TabIndex = 3;
@@ -93,8 +100,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 197);
+            this.label2.Location = new System.Drawing.Point(64, 261);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(155, 13);
             this.label2.TabIndex = 4;
@@ -102,18 +110,21 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 26);
+            this.label1.Size = new System.Drawing.Size(212, 40);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Pronunciation Lexicons for Any Low-Resource Language";
+            this.label1.Text = "Pronunciation Lexicons for Any Low-resource Language";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 311);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "StartForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
