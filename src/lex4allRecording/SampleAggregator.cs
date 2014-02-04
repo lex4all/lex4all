6d4085bf175911177579052582f7df3afd67ca76
Max Paulus <maxpaulus@web.de>
2014-02-04 15:47:33 +0100

    recording extra files

---
+++ src/lex4allRecording/SampleAggregator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lex4allRecording
+{
+    public class SampleAggregator
+    {
+        private float maxSample;
+        private float minSample;
+        private int counter;
+
+        public SampleAggregator()
+        {
+            maxSample = 0;
+            minSample = 0;
+            counter = 100;
+        }
+
+        public int passSample(float sample) {
+            int flag = 0;
+
+            if(sample > maxSample) {
+                maxSample = sample;
+            } else if(sample < minSample) {
+                minSample = sample;
+            }
+
+            if (counter == 0)
+            {
+                flag = 1;
+                counter = 100;
+            }
+
+            counter = counter-1;
+            return flag;
+        }
+
+        public float MaxSample
+        {
+            get { return maxSample; }
+            set { maxSample = value; }
+        }
+
+        public float MinSample
+        {
+            get { return minSample; }
+            set { minSample = value; }
+        }
+    }
+}
---
+++ src/lex4allRecording/SampleEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lex4allRecording
+{
+    public class SampleEventArgs : EventArgs
+    {
+        public SampleEventArgs(float sample)
+        {
+            sample32 = sample;
+        }
+
+        public float sample32;
+
+        public float Sample32
+        {
+            get { return sample32; }
+            set { sample32 = value; }
+        }
+
+    }
+}
